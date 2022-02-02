6b608712f96db7a46bfc71269ba49d8795193aa5
SpaceCat <57956840+lSpaceCatl@users.noreply.github.com>
2022-02-03 01:54:33 +0300

    Update answers-script.user.js

--- answers-script.user.js
+++ answers-script.user.js
@@ -8,8 +8,8 @@
 // @match       *://online-edu.mirea.ru/*
 // @match       *://oniel.beget.tech/*
 // @match       *://dl.sechenov.ru/*
-// @updateURL   https://raw.githubusercontent.com/Ninja-Official/moodle-answers-script/main/answers-script.meta.js
-// @downloadURL https://raw.githubusercontent.com/Ninja-Official/moodle-answers-script/main/answers-script.user.js
+// @updateURL   https://raw.githubusercontent.com/lSpaceCatl/answer/main/answers-script.user.js
+// @downloadURL https://raw.githubusercontent.com/lSpaceCatl/answer/main/answers-script.user.js
 // @supportURL  https://mirea.ninja/t/novaya-versiya-skripta-dlya-obmena-otvetami-v-testirovaniya-v-sdo/486
 // @require     https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3/dist/fp.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
